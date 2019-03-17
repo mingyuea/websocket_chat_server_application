11094450cd2a6408b19100092613a01c5bde4895
unknown <mingyuea@gmail.com>
2019-03-16 17:57:33 -0700

    fixed newline bug

--- application/app.js
+++ application/app.js
@@ -1,39 +1,51 @@
 const http = require('http');
 const static = require('node-static');
 const crypto = require('crypto');
-const file = new static.Server('./static');
+const fileServer = new static.Server('./application');
 
 const PORT = 5000;
 
 const server = http.createServer((req, res) => {
-	req.addListner('end', () => file.server(req, res)).resume();
+	req.addListener('end', () => fileServer.serve(req, res)).resume();
 });
 
 server.on('upgrade', (req, socket) => {
-	console.log(req);
+	//console.log(req);
 
-	if(req.headers['Upgrade'] !== "websocket"){
+	if(req.headers['upgrade'] !== "websocket"){
 		socket.end('HTTP/1.1 400 Bad Request');
 		return;
 	}
 
-	const acceptKey = req.headers['sec-websocket-key'];
+	let acceptKey = req.headers['sec-websocket-key'];
 	const acceptHash = generateValue(acceptKey);
+
+	console.log('acceptkey', acceptKey, 'hash', acceptHash, 'nk', newKey);
+	
 	const resHeaders = [ 'HTTP/1.1 101 Web Socket Protocol Handshake', 'Upgrade: WebSocket', 'Connection: Upgrade', `Sec-WebSocket-Accept: ${acceptHash}` ];
 	
+
 	let protocols = req.headers['sec-websocket-protocol'];
 	protocols = !protocols ? [] : protocols.split(',').map(name => name.trim());
 
-	if(protocols.include('json')){
-		resHeaders.push(`Sec-WebSocket-Protocol: json`);
+	if(protocols.includes('json')){
+		console.log('json here');
+		resHeaders.push('Sec-WebSocket-Protocol: json');
 	}
 
-	socket.write(responseHeaders.join('\r\n') + '\r\n\r\n');
+	console.log(resHeaders);
+
+	socket.write(resHeaders.join('\r\n')/* + '\r\n\r\n'*/);
+	//console.log(socket);
 })
 
 function generateValue(key){
-	let acceptVal = crypto.createHash('sha1').update(key + '258EAFA5-E914–47DA-95CA-C5AB0DC85B11', 'binary').digest('base64');
-	return acceptVal;
+	//let acceptVal = crypto.createHash('sha1').update(key + '258EAFA5-E914–47DA-95CA-C5AB0DC85B11', 'binary').digest('base64');
+	//return acceptVal;
+	return crypto
+	  .createHash('sha1')
+	  .update(key + '258EAFA5-E914–47DA-95CA-C5AB0DC85B11', 'binary')
+	  .digest('base64');
 }
 
 server.listen(PORT, () => console.log("Node server is running on PORT " + PORT));
